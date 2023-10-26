170d76752bed27ec1c611634100c47f7e64f5e32
clopezldam1 <clopezldam1@gmail.com>
2023-10-26 10:50:25 +0200

    cuarto commit desde clase

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -8,6 +8,10 @@ public class Player2 : MonoBehaviour
     float horizontal;
     float vertical;
     float speed = 5f;
+    float salto;
+    float timerSalto;
+    float alturaSalto = 100f; //f es unidad de fuerza - basada en newtons, es en relacion a cuanto te hace falta para levantar el cuerpo (aka, peso del rigbody)
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +31,17 @@ public class Player2 : MonoBehaviour
         Vector2 velocityVertical = rb2d.velocity;
         velocityVertical.y = vertical * speed;
         rb2d.velocity = velocityVertical;
+
+        //para que salte
+        salto = Input.GetAxis("Jump"); //returns 1 si tecla esta pusalda
+        Debug.Log("Salto: " + salto);
+        timerSalto -= Time.deltaTime;
+        if (timerSalto <= 0 && salto == 1)
+        { //si timer es 0 puede saltar y si pulsa tecla salto
+            //? no sé qué poner en el if para que solo salte 1 vez (que sin if vuela)
+            timerSalto = 3; //limitador: sollo puede salatr una vez cada 3 segundos, si mantienes pulsado vuelas
+            rb2d.AddForce(new Vector2(0, salto * alturaSalto));
+        }
     }
 
 }
